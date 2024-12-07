6172fa24f0a37e5e00c245fc2ebd0232bf20ae7c
realsahilsaini <thisissahilsaini@gmail.com>
2024-12-07 18:48:34 +0530

    API: All stats

--- Backend/routes/stats.js
+++ Backend/routes/stats.js
@@ -3,7 +3,7 @@ const statsRouter = Router();
 
 const DataModel = require('../models/seedData');
 
-statsRouter.get('/', async (req,res)=>{
+statsRouter.get('/sales', async (req,res)=>{
   
   try{
     const {month, year} = req.query;
@@ -117,6 +117,110 @@ statsRouter.get('/bar-chart', async (req,res)=>{
     })
   }
 
+})
+
+
+statsRouter.get('/pie-chart', async (req,res)=>{
+  try{
+    const {month} = req.query;
+
+    if(!month || isNaN(month) || month < 1 || month > 12){
+      return res.status(400).json({
+        message: "A valid month (1-12) is required"
+      })
+    }
+
+    const selectedMonth = parseInt(month, 10);
+
+    const data = await DataModel.aggregate([
+      {
+        $addFields:{
+          saleMonth: {$month: "$dateOfSale"},
+        },
+      },
+      {
+        $match:{
+          saleMonth: selectedMonth,
+        },
+      },
+      {
+        $group:{
+          _id: '$category',
+          count: {$sum: 1}
+        },
+      },
+      {
+        $project:{
+          category: '$_id',
+          count: 1,
+          _id: 0
+        },
+      },
+    ])
+
+
+    res.status(200).json({data});
+
+  }catch(err){
+    res.status(500).json({
+      message: "Error fetching data",
+      error: err
+    })
+  }
+})
+
+
+//make an api to get result of the pie chart, bar chart and the total sales combined
+statsRouter.get('/all-stats', async (req, res)=>{
+  try{
+
+    const {month, year} = req.query;
+
+    if(!month || !year){
+      return res.status(400).json({
+        message: "Month and Year are required"
+      })
+    }
+
+    //Base Url
+    // const baseUrl = `${req.protocol}://${req.get('host')}`;
+    const baseUrl = 'http://localhost:3000';
+
+    //API Endpoints
+    const salesStatsUrl = `${baseUrl}/api/v1/stats/sales?month=${month}&year=${year}`;
+    const barChartUrl = `${baseUrl}/api/v1/stats/bar-chart?month=${month}`;
+    const pieChartUrl = `${baseUrl}/api/v1/stats/pie-chart?month=${month}`;
+
+
+    //Fetch all data concurrently
+    const salesStatsResponse = await fetch(salesStatsUrl);
+    const barChartResponse = await fetch(barChartUrl);
+    const pieChartResponse = await fetch(pieChartUrl);
+
+    //Parse the response
+    const salesStatsData = await salesStatsResponse.json();
+    const barChartData = await barChartResponse.json();
+    const pieChartData = await pieChartResponse.json();
+
+    //Combine the data
+    const combinedData = {
+      salesStats: salesStatsData,
+      barChart: barChartData,
+      pieChart: pieChartData
+    }
+
+
+    res.status(200).json({
+      message: "Data fetched successfully",
+      data: combinedData
+    });
+
+  }catch(err){
+    res.status(500).json({
+      message: "Error fetching data",
+      error: err
+    })
+  }
 })
 
 
